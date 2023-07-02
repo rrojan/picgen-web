3aca299c069941e65ae3644a2bc71f2f55370aef
Rojan Rajbhandari <rrojan20@gmail.com>
2023-07-02 20:59:56 +0545

    refactor: clean code

--- generator/api.py
+++ generator/api.py
@@ -1,13 +1,19 @@
 from random import choice
 from django.http import JsonResponse
-from .models import GeneratedPhoto, Photo
 from django.utils import timezone
+from .models import GeneratedPhoto, Photo
 
 
 def photos(request):
     photos = GeneratedPhoto.objects.all().order_by('-created_at')
-    data = [{'title': photo.title, 'image': photo.image.url, 'created_at': timezone.localtime(photo.created_at).strftime('%Y-%m-%d %H:%M:%S')}
-            for photo in photos]
+    data = [
+        {
+            'title': photo.title,
+            'image': photo.image.url,
+            'created_at': timezone.localtime(photo.created_at).strftime('%Y-%m-%d %H:%M:%S')
+        }
+        for photo in photos
+    ]
     return JsonResponse(data, safe=False)
 
 
@@ -17,10 +23,9 @@ def random_photo(request):
         random_photo = choice(photos)
         # Add this generated photo to the list of generated photos
         GeneratedPhoto.build_from_photo(random_photo)
-        created_at_gmt = timezone.localtime(random_photo.created_at)
-        created_at_gmt = created_at_gmt.replace(tzinfo=timezone.timezone.utc)
-        created_at_gmt = created_at_gmt.astimezone(
-            timezone.get_fixed_timezone(345))  # GMT+5:45
+        created_at_gmt = timezone.localtime(random_photo.created_at).replace(
+            tzinfo=timezone.timezone.utc
+        ).astimezone(timezone.get_fixed_timezone(345))  # GMT+5:45
         data = {
             'title': random_photo.title,
             'image': random_photo.image.url,
